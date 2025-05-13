aee56d3c40a0d1436030c46a8b21c0811a2476b0
vaneavasco <vaneavasco@outlook.com>
2025-05-13 11:35:50 +0300

    :wrench: fix tests by updating jest config

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,9 @@ export default {
     preset: 'ts-jest/presets/default-esm',
     testEnvironment: 'node',
     moduleNameMapper: {
-        '^(\\.{1,2}/.*)\\.js$': '$1'
+        '^(\\.{1,2}/.*)\\.js$': '$1', // Handle relative imports with `.js` extensions
+        '^@/(.*)\\.js$': '<rootDir>/src/$1.ts',
+        '^@/(.*)$': '<rootDir>/src/$1'
     },
     transform: {
         '^.+\\.tsx?$': [
